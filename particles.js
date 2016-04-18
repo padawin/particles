c0bb1d31af26f95f845cfe967efdfe381eb19418
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-18 07:49:11 +0100

    class for the particles added
    the opacity must be between 0 and 1, not 0 and 100

--- particles.js
+++ particles.js
@@ -1,4 +1,37 @@
 (function () {
 	var canvas = document.getElementById('myCanvas'),
 		canvasContext = canvas.getContext('2d');
+
+	const PARTICLE_STATES = {
+		DEAD: 0,
+		ALIVE: 1
+	};
+	const PARTICLE_RADIUS = 5;
+
+	function Particle (position, speed, maxLife) {
+		this.life = maxLife;
+		this.maxLife = maxLife;
+		this.position = position;
+		this.speed = speed;
+		this.state = PARTICLE_STATES.ALIVE;
+	}
+
+	Particle.prototype.update = function () {
+		this.life--;
+
+		if (this.life == 0) {
+			this.state = PARTICLE_STATES.DEAD;
+		}
+
+		this.position.x += this.speed.x;
+		this.position.y += this.speed.y;
+	};
+
+	Particle.prototype.draw = function () {
+		var opacity = this.life / this.maxLife;
+		canvasContext.fillStyle = 'rgba(123, 43, 109, ' + opacity + ')';
+		canvasContext.beginPath();
+		canvasContext.arc(this.position.x, this.position.y, PARTICLE_RADIUS, 0, 2 * Math.PI, true);
+		canvasContext.fill();
+	};
 })();
